c32b8f80604c1a6dc91cf4af9fde23bf08e44a99
Maksim Gluwenko <maksim_gluwenko@epam.com>
2025-03-02 11:04:03 +0500

    Remove part that disables frontend container because now it lvie updates

--- scripts/run_local_test_stack_without_plesk_access.sh
+++ scripts/run_local_test_stack_without_plesk_access.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker compose up -d && docker stop $(docker ps -a -q --filter ancestor=backend:latest --format="{{.ID}}") && docker stop $(docker ps -a -q --filter ancestor=frontend:latest --format="{{.ID}}") && source backend/.venv/bin/activate && (cd backend && fastapi run 'app/run_local_stack_without_plesk_access.py')
+docker compose up -d && docker stop $(docker ps -a -q --filter ancestor=backend:latest --format="{{.ID}}") && source backend/.venv/bin/activate && (cd backend && fastapi run 'app/run_local_stack_without_plesk_access.py')
