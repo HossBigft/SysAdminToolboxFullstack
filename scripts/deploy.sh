390103dc3c540d06417c37ce624d1aa9d8d85137
Maksim Gluwenko <maksim_gluwenko@epam.com>
2025-05-05 17:35:20 +0500

    Remove custom stack name

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,11 +2,5 @@
 
 # Exit in case of error
 set -e
-. "$(dirname "$0")/load_dotenv.sh"
 
-# Enable debug with DEBUG=1 ./deploy.sh
-load_dotenv
-
-# Check required variables
-STACK_NAME=${STACK_NAME?Variable not set} \
-docker compose  -f docker-compose.yml  -p "$STACK_NAME" up -d
+docker compose  -f docker-compose.yml up -d
