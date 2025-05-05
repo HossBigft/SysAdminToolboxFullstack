f7c99bc4b2828e5bb09acc4ebff0ee8c26e4f4fb
Maksim Gluwenko <maksim_gluwenko@epam.com>
2025-05-05 11:52:15 +0500

    Modify deploy script to use dotevn

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,19 @@
-#! /usr/bin/env sh
+#!/usr/bin/env sh
 
 # Exit in case of error
 set -e
+. "$(dirname "$0")/load_dotenv.sh"
 
+# Enable debug with DEBUG=1 ./deploy.sh
+load_dotenv
+
+# Check required variables
 DOMAIN=${DOMAIN?Variable not set} \
 STACK_NAME=${STACK_NAME?Variable not set} \
-TAG=${TAG?Variable not set} \
+TAG=${TAG=-latest} \
 docker-compose \
--f docker-compose.yml \
-config > docker-stack.yml
+    -f docker-compose.yml \
+    config > docker-stack.yml
 
 docker-auto-labels docker-stack.yml
 
