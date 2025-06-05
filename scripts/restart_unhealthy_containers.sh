28be197c4f36cb625a3181758f6274882aa1a270
Maxim <m.glushchenko@office.hoster.kz>
2025-06-05 09:12:53 +0500

    Add sudo to script

--- scripts/restart_unhealthy_containers.sh
+++ scripts/restart_unhealthy_containers.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Get a list of unhealthy containers
-unhealthy_containers=$(docker ps -q -f health=unhealthy)
+unhealthy_containers=$(sudo docker ps -q -f health=unhealthy)
 # Loop through the unhealthy containers and restart them
 for container_id in $unhealthy_containers; do
-  container_name=$(docker inspect --format='{{.Name}}' "$container_id" | cut -c2-) 
+  container_name=$(sudo docker inspect --format='{{.Name}}' "$container_id" | cut -c2-) 
   echo "[$(date)] Restarting unhealthy container: $container_name ($container_id)"
-  docker restart "$container_id"
+  sudo docker restart "$container_id"
 done
