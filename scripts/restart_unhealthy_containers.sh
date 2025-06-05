13b6062d138d182944541be119c2c24f3300fc33
Maxim <m.glushchenko@office.hoster.kz>
2025-06-05 09:02:05 +0500

    Add script to restart unhealthy containers

---
+++ scripts/restart_unhealthy_containers.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Get a list of unhealthy containers
+unhealthy_containers=$(docker ps -q -f health=unhealthy)
+# Loop through the unhealthy containers and restart them
+for container_id in $unhealthy_containers; do
+echo "Restarting unhealthy container: $container_id"
+docker restart "$container_id"
+done
