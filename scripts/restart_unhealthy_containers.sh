0b6115e9f713c5f08a12c1842f4534c9be295a58
Maxim <m.glushchenko@office.hoster.kz>
2025-06-05 09:02:05 +0500

    Add script to restart unhealthy containers

--- scripts/restart_unhealthy_containers.sh
+++ scripts/restart_unhealthy_containers.sh
@@ -3,6 +3,7 @@
 unhealthy_containers=$(docker ps -q -f health=unhealthy)
 # Loop through the unhealthy containers and restart them
 for container_id in $unhealthy_containers; do
-echo "Restarting unhealthy container: $container_id"
-docker restart "$container_id"
+  container_name=$(docker inspect --format='{{.Name}}' "$container_id" | cut -c2-) 
+  echo "[$(date)] Restarting unhealthy container: $container_name ($container_id)"
+  docker restart "$container_id"
 done
